ea77ad3326790a6fc370c453a692e58c980166fc
STEVENZHO <139161600+STEVENZHO@users.noreply.github.com>
2023-07-31 15:25:46 -0400

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,12 +2,13 @@ import asyncio
 from bleak import BleakClient
 import time
 
-address = "E0:5A:1B:CB:03:92"  # replace with your device address
+addresses = ["E0:5A:1B:CA:00:42", "E0:5A:1B:CB:03:92"]  # replace with your devices addresses
 characteristic_uuid = "beb5483e-36e1-4688-b7f5-ea07361b26a8"  # replace with your characteristic UUID
-total_bytes_received = 0
+
+total_bytes_received = [0, 0]
 start_time = None
 
-async def run(address, loop):
+async def run(address, index, loop):
     global total_bytes_received, start_time
 
     client = BleakClient(address, loop=loop)
@@ -16,13 +17,13 @@ async def run(address, loop):
         await client.connect()
         start_time = time.time()
 
-        while total_bytes_received < 1e6:  # while total data received is less than 1 MB
+        while total_bytes_received[index] < 1e6:  # while total data received is less than 1 MB
             value = await client.read_gatt_char(characteristic_uuid)
-            total_bytes_received += len(value)
+            total_bytes_received[index] += len(value)
             current_time = time.time()
             elapsed_time = current_time - start_time
-            transfer_rate = (total_bytes_received / elapsed_time) / 1024  # Convert to KB/s
-            print(f"Total data received: {total_bytes_received / 1024} KB, Transfer rate: {transfer_rate} KB/s")  # short delay to avoid flooding
+            transfer_rate = (total_bytes_received[index] / elapsed_time) / 1024  # Convert to KB/s
+            print(f"Device {index} - Total data received: {total_bytes_received[index] / 1024} KB, Transfer rate: {transfer_rate} KB/s")  # short delay to avoid flooding
             
     except Exception as e:
         print(e)
@@ -32,4 +33,6 @@ async def run(address, loop):
             await client.disconnect()
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(run(address, loop))
+
+# Run the run function for each address concurrently
+loop.run_until_complete(asyncio.gather(*(run(address, i, loop) for i, address in enumerate(addresses))))
