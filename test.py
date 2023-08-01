1312a4fff02bf0ded3099cced28cd5f56ae24b85
STEVENZHO <139161600+STEVENZHO@users.noreply.github.com>
2023-08-01 18:20:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,45 +2,34 @@ import asyncio
 from bleak import BleakClient
 import time
 
-address = "E0:5A:1B:CB:03:92"  # replace with your device address
+address = "E0:5A:1B:CA:00:42"  # replace with your device address
 characteristic_uuid = "beb5483e-36e1-4688-b7f5-ea07361b26a8"  # replace with your characteristic UUID
 total_bytes_received = 0
 start_time = None
-MB = 1048576  # 1MB in bytes
 
 async def run(address, loop):
     global total_bytes_received, start_time
 
     client = BleakClient(address, loop=loop)
+    
+    try:
+        await client.connect()
+        start_time = time.time()
 
-    while True:
-        try:
-            await client.connect()
-            start_time = time.time()
-            while True:
-                if total_bytes_received >= MB:  # If received 1MB or more, break loop
-                    break
-                value = await client.read_gatt_char(characteristic_uuid)
-                total_bytes_received += len(value)
-                current_time = time.time()
-                elapsed_time = current_time - start_time
-                transfer_rate = (total_bytes_received / elapsed_time) / 1024  # Convert to KB/s
-                print(f"Data received: {len(value)} bytes, Total data: {total_bytes_received} bytes, Transfer rate: {transfer_rate} KB/s")
-                await asyncio.sleep(1)  # Sleep for a while before next reading
+        while total_bytes_received < 1e6:  # while total data received is less than 1 MB
+            value = await client.read_gatt_char(characteristic_uuid)
+            total_bytes_received += len(value)
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            transfer_rate = (total_bytes_received / elapsed_time) / 1024  # Convert to KB/s
+            print(f"Total data received: {total_bytes_received / 1024} KB, Transfer rate: {transfer_rate} KB/s")  # short delay to avoid flooding
+            
+    except Exception as e:
+        print(e)
 
-        except Exception as e:
-            print(e)
-            pass
-
-        finally:
-            if client.is_connected:
-                await client.disconnect()
-
-        if total_bytes_received >= MB:  # If received 1MB or more, break loop
-            break
-
-    total_time = time.time() - start_time
-    print(f"Time taken to receive 1MB of data: {total_time} seconds")
+    finally:
+        if client.is_connected:
+            await client.disconnect()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run(address, loop))
