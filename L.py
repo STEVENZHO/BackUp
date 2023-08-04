335a67dab76e9c50d725c7d0da7159d12f308851
STEVENZHO <139161600+STEVENZHO@users.noreply.github.com>
2023-08-04 15:28:20 -0400

    Update L.py

--- L.py
+++ L.py
@@ -1,55 +1,41 @@
-from bluepy.btle import Peripheral, DefaultDelegate
+from bluepy.btle import Peripheral, DefaultDelegate, BTLEException
 import time
-import threading
-
-addresses = ["E0:5A:1B:CA:00:42", "E0:5A:1B:CB:03:92", "44:09:E7:36:CE:8F", "01:2F:7B:97:7E:AF"]
-characteristic_uuid = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-total_bytes_received = [0, 0, 0, 0]
-start_time = None
 
 class MyDelegate(DefaultDelegate):
-    def __init__(self, index):
+    def __init__(self):
         DefaultDelegate.__init__(self)
-        self.index = index
 
     def handleNotification(self, cHandle, data):
         global total_bytes_received, start_time
-        total_bytes_received[self.index] += len(data)
+        total_bytes_received += len(data)
         current_time = time.time()
         elapsed_time = current_time - start_time
-        transfer_rate = (total_bytes_received[self.index] / elapsed_time) / 1024
-        print(f"Device {self.index} - Total data received: {total_bytes_received[self.index] / 1024} KB, Transfer rate: {transfer_rate} KB/s")
+        transfer_rate = (total_bytes_received / elapsed_time) / 1024  # Convert to KB/s
+        print(f"Total data received: {total_bytes_received / 1024} KB, Transfer rate: {transfer_rate} KB/s") 
 
-def run(address, index):
-    global start_time
-    p = Peripheral()
-    p.setDelegate(MyDelegate(index))
+addresses = ["E0:5A:1B:CA:00:42", "E0:5A:1B:CB:03:92", "44:09:E7:36:CE:8F", "01:2F:7B:97:7E:AF"]  # replace with your devices addresses
+total_bytes_received = 0
+start_time = time.time()
 
+for index, address in enumerate(addresses):
     try:
-        p.connect(address)
-        start_time = start_time or time.time()
-        svc = p.getServiceByUUID(characteristic_uuid)
+        p = Peripheral(address)
+        p.setDelegate(MyDelegate())
 
-        while total_bytes_received[index] < 1e6:
-            if p.waitForNotifications(1.0):
-                continue
-
-            print("Waiting...")
-
-    except Exception as e:
-        print(e)
-
-    finally:
+        services=p.getServices()
+        for service in services:
+            if service.uuid == "4fafc201-1fb5-459e-8fcc-c5c9c331914b":
+                characteristics = service.getCharacteristics()
+                for characteristic in characteristics:
+                    if characteristic.uuid == "beb5483e-36e1-4688-b7f5-ea07361b26a8":
+                        while total_bytes_received < 1e6: # while total data received is less than 1 MB
+                            data = characteristic.read()
+                            total_bytes_received += len(data)
+                            current_time = time.time()
+                            elapsed_time = current_time - start_time
+                            transfer_rate = (total_bytes_received / elapsed_time) / 1024  # Convert to KB/s
+                            print(f"Device {index} - Total data received: {total_bytes_received / 1024} KB, Transfer rate: {transfer_rate} KB/s") 
         p.disconnect()
 
-# Run the run function for each address concurrently
-threads = []
-
-for i, address in enumerate(addresses):
-    thread = threading.Thread(target=run, args=(address, i))
-    threads.append(thread)
-    thread.start()
-
-# Wait for all threads to complete
-for thread in threads:
-    thread.join()
+    except BTLEException as e:
+        print(e)
