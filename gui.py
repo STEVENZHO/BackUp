66488cc9e941a5ce1fe67157e159730943b4c177
STEVENZHO <139161600+STEVENZHO@users.noreply.github.com>
2023-08-10 14:33:12 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys
-import numpy as np
 import matplotlib.pyplot as plt
+from qasync import QEventLoop, asyncSlot
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QGraphicsScene
 from PyQt5.QtGui import QPixmap
@@ -50,9 +50,11 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        self.pushButton_2.clicked.connect(self.scan)
-        self.listView.clicked.connect(self.on_device_clicked)
-        self.pushButton.clicked.connect(self.save_graph)
+
+        self.pushButton_2.clicked.connect(lambda: asyncio.ensure_future(self.scan()))
+        self.listView.clicked.connect(lambda index: asyncio.ensure_future(self.on_device_clicked(index)))
+        self.pushButton.clicked.connect(lambda: self.save_graph())
+
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -60,47 +62,39 @@ class Ui_MainWindow(object):
         self.pushButton_2.setText(_translate("MainWindow", "scan"))
         self.pushButton.setText(_translate("MainWindow", "save"))
 
-    def scan(self):
-        async def run():
-            scanner = BleakScanner()
-            self.devices = await scanner.discover()
-            
-            device_list = [f"{device.name}" for device in self.devices]
-            
-            model = QStringListModel()
-            model.setStringList(device_list)
-            self.listView.setModel(model)
+    async def scan(self):
+        scanner = BleakScanner()
+        self.devices = await scanner.discover()
+        
+        device_list = [device.name for device in self.devices]
+        
+        model = QStringListModel()
+        model.setStringList(device_list)
+        self.listView.setModel(model)
 
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(run())
-
-    def on_device_clicked(self, index: QModelIndex):
+    async def on_device_clicked(self, index: QModelIndex):
         selected_indexes = self.listView.selectionModel().selectedIndexes()
         for index in selected_indexes:
             device = self.devices[index.row()]
             if device.name not in self.devices_data:
                 self.devices_data[device.name] = []
                 print(f"Attempting to connect to {device.name} at {device.address}")
-                self.connect_and_listen(device)
+                await self.connect_and_listen(device)
 
-    def connect_and_listen(self, device):
-        async def connect_to_device():
-            client = BleakClient(device.address)
-            try:
-                await client.connect()
-                if client.is_connected:
-                    print(f"Connected to {device.name}")
-                    await self.listen_to_data(client, device.name)
-                else:
-                    print(f"Failed to connect to {device.name}")
-            except BleakError as e:
-                print(f"Error: {e}")
-            finally:
-                if client.is_connected:
-                    await client.disconnect()
-
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(connect_to_device())
+    async def connect_and_listen(self, device):
+        client = BleakClient(device.address)
+        try:
+            await client.connect()
+            if client.is_connected:
+                print(f"Connected to {device.name}")
+                await self.listen_to_data(client, device.name)
+            else:
+                print(f"Failed to connect to {device.name}")
+        except BleakError as e:
+            print(f"Error: {e}")
+        finally:
+            if client.is_connected:
+                await client.disconnect()
 
     async def listen_to_data(self, client, device_name):
         while client.is_connected:
@@ -129,8 +123,13 @@ class Ui_MainWindow(object):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    sys.exit(app.exec_())
+
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
+    with loop:
+        loop.run_forever()
